e0d7550c1d1bd5b927b12702a255d5cfbd2a375f
joname1 <274158741@qq.com>
2021-08-07 16:22:21 +0800

    dfsdfgsdfg

---
+++ bundle.js
@@ -0,0 +1,146 @@
+    $(document).ready(function () {
+        var font = new FontFaceObserver('Simple');
+        font.load(null, 50000).then(function () {
+            $("#submit").show();
+        });
+    });
+
+    toastr.options = {
+        "debug": false,
+        "positionClass": "toast-center-center",
+        "onclick": null,
+        "fadeIn": 300,
+        "fadeOut": 1000,
+        "timeOut": 2000,
+        "extendedTimeOut": 1000
+    }
+
+    var colo = "#18140F";
+    var fonts = "38px Simple";
+
+    var canv = document.getElementById("myCanvas");
+    var ctx = canv.getContext("2d");
+    var imageUpload = document.getElementById('imageUpload');
+    // var img2 = document.createElement("img");
+    // img2.src = $('#upload').val();
+    var imgInstance;
+
+    function readUrl(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#upload').val(e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    // imageUpload.onchange = function () {
+    //     readUrl(this);
+    // }
+
+    var btn = document.getElementById('submit');
+    var screenBox = document.getElementById('makePic');
+    var img = document.createElement("img");
+    btn.onclick = function (event) {
+        $("#submit").addClass("disabled");
+        screenBox.src = '';
+        var name1 = $('#input-name').val();
+        var name2 = $('#input-sex').val();
+        var name3 = $('#input-idn').val();
+        var name4 = $('#input-age').val() + '岁';
+        var name5 = $('#input-date').val();
+        var name6 = Math.round(Math.random() * 1000) < 10 ? '9210710000' + Math.round(Math.random() * 1000) :
+            '921071000' + Math.round(Math.random() * 1000);
+        var name7 = $('#input-address').val();
+        var name8 = Math.round(Math.random() * 1000) < 10 ? '00' + Math.round(Math.random() * 1000) + '-2' : Math
+            .round(Math.random() * 1000) + '-2';
+        if (isNull(name1)) {
+            toastr.error("请填写姓名!");
+            $("#submit").removeClass("disabled");
+            return false;
+        }
+        // var timestamp = Date.parse(new Date());
+        // img2.src = $('#upload').val();
+        img = new Image();
+        img.crossOrigin = "*";
+        img.src = $('#local').val();
+        img.onload = function (canvas) {
+            canv.width = img.width;
+            canv.height = img.height;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+            // var scale = img2.height / img2.width;
+            // var cheight = 160 * scale;
+            // ctx.drawImage(img2, 290, 35, 160, cheight);
+            setName(ctx, 50, 110, name5); //date
+
+            setName(ctx, 180, 270, name1); //name
+            setName(ctx, 180, 320, name2); //sex
+            setName(ctx, 180, 365, name4); //age
+
+            setName(ctx, 1055, 270, name3); //id-number
+            setName(ctx, 1055, 320, name6); //id
+            setName(ctx, 1055, 365, name8); //sample-id
+
+            setName(ctx, 2025, 270, name5 + ' ' + '16:37'); //t1
+            setName(ctx, 2025, 320, name5 + ' ' + '19:58'); //t2
+            setName(ctx, 2025, 365, name5 + ' ' + '19:58'); //t3
+
+            setName(ctx, 1410, 1513, name5 + ' ' + '23:15'); //t4
+            setName(ctx, 2030, 1513, name5 + ' ' + '23:19'); //t5
+            setName2(ctx);
+            var dataUrl = canv.toDataURL('image/jpg', 1);
+            screenBox.src = dataUrl;
+        }
+        $("#submit").removeClass("disabled");
+        $("#modal-demo").attr("class", "modal active");
+
+        function setName(context, left, top, name) {
+            context.shadowColor = 'rgba(0, 0, 0, 0.4)';
+            context.shadowBlur = 2;
+            context.fillStyle = colo;
+            context.font = fonts;
+            context.textAlign = "left";
+            var lineWidth = 0;
+            var initHeight = top;
+            var lastSubStrIndex = 0;
+            context.fillText(name.substring(lastSubStrIndex, name.length), left, initHeight);
+            context.fill();
+        }
+
+        function setName2(context) {
+            context.shadowColor = 'rgba(0, 0, 0, 0.4)';
+            context.shadowBlur = 2;
+            context.fillStyle = colo;
+            context.font = fonts;
+            var lineWidth = 0;
+            var canvasWidth = 190; //计算canvas的宽度
+            var initHeight = 168; //绘制字体距离canvas顶部初始的高度
+            var lastSubStrIndex = 0; //每次开始截取的字符串的索引
+            for (var i = 0; i < name7.length; i++) {
+                lineWidth += context.measureText(name7[i]).width;
+                if (lineWidth > canvasWidth) {
+                    context.fillText(name7.substring(lastSubStrIndex, i), '100', initHeight); //绘制截取部分
+                    initHeight += 23; //25为字体的高度
+                    lineWidth = 0;
+                    canvasWidth = 190;
+                    lastSubStrIndex = i;
+                }
+                if (i == name7.length - 1) { //绘制剩余部分
+                    context.fillText(name7.substring(lastSubStrIndex, i + 1), '100', initHeight);
+                }
+            }
+            context.stroke();
+        }
+
+        function isNull(str) {
+            if (str == "") return true;
+            var regu = "^[ ]+$";
+            var re = new RegExp(regu);
+            return re.test(str);
+        }
+
+    }
+
+    function closegif() {
+        $("#modal-demo").attr("class", "modal");
+    }
